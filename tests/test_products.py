6eb6f372bebba4fad422b34c205a10279943f9f1
Jason Shepherd <jshepher@redhat.com>
2025-04-07 16:00:25 +1000

    adjust consolidation so that it deduplicates branches, and returns a list of results, instead of a single tree

--- tests/test_products.py
+++ tests/test_products.py
@@ -4,9 +4,9 @@ from anytree import Node
 
 from trustshell.products import (
     _build_node_purl,
-    _build_root_tree,
-    _consolidate_duplicate_nodes,
+    _trees_with_cpes,
     _render_tree,
+    _has_cpe_node,
 )
 
 
@@ -37,74 +37,86 @@ def test_build_node_purl_oci():
     assert result == "pkg:oci/quay?tag=v3.12.8-1"
 
 
-def test_build_root_tree_srpm():
-    base_purl = "pkg:rpm/redhat/openssl"
+def test_trees_with_cpes_srpm():
     with open("tests/testdata/openssl.json", "r") as file:
         data = json.load(file)
-    result = _build_root_tree(base_purl, data)
-    _render_tree(result)
-    assert result.name == base_purl
-    _check_node_names_at_depth(result, 1, ["pkg:rpm/redhat/openssl@3.0.7-18.el9_2"])
+    result = _trees_with_cpes(data)
+    assert len(result) == 1
+    _render_tree(result[0])
+    assert result[0].name == "pkg:rpm/redhat/openssl@3.0.7-18.el9_2"
     expected_cpes = [
         "cpe:/a:redhat:rhel_eus:9.2:*:appstream:*",
         "cpe:/a:redhat:rhel_eus:9.2:*:baseos:*",
     ]
-    _check_node_names_at_depth(result, 2, expected_cpes)
+    _check_node_names_at_depth(result[0], 1, expected_cpes)
 
 
-def test_build_root_tree_binary_rpm():
-    base_purl = "pkg:rpm/redhat/openssl"
+def test_trees_with_cpes_binary_rpm():
     with open("tests/testdata/openssl-libs.json", "r") as file:
         data = json.load(file)
-    result = _build_root_tree(base_purl, data)
-    _render_tree(result)
-    _check_node_names_at_depth(
-        result, 1, ["pkg:rpm/redhat/openssl-libs@3.0.7-18.el9_2"]
-    )
-    _check_node_names_at_depth(result, 2, ["pkg:rpm/redhat/openssl@3.0.7-18.el9_2"])
+    result = _trees_with_cpes(data)
+    assert len(result) == 1
+    _render_tree(result[0])
+    assert result[0].name == "pkg:rpm/redhat/openssl-libs@3.0.7-18.el9_2"
+    _check_node_names_at_depth(result[0], 1, ["pkg:rpm/redhat/openssl@3.0.7-18.el9_2"])
     expected_cpes = [
         "cpe:/a:redhat:rhel_eus:9.2:*:appstream:*",
         "cpe:/a:redhat:rhel_eus:9.2:*:baseos:*",
     ]
-    _check_node_names_at_depth(result, 3, expected_cpes)
+    _check_node_names_at_depth(result[0], 2, expected_cpes)
 
 
-def test_build_root_tree_container_cdx():
-    base_purl = "pkg:oci/quay-builder-qemu-rhcos-rhel-8"
+def test_trees_with_cpes_container_cdx():
     with open("tests/testdata/quay-builder-qemu-rhcos-rhel-8.json") as file:
         data = json.load(file)
-    result = _build_root_tree(base_purl, data)
-    _render_tree(result)
-    assert result.name == base_purl
-    _check_node_names_at_depth(
-        result, 1, ["pkg:oci/quay-builder-qemu-rhcos-rhel8?tag=v3.12.8-1"]
-    )
+    result = _trees_with_cpes(data)
+    assert len(result) == 1
+    _render_tree(result[0])
+    assert result[0].name == "pkg:oci/quay-builder-qemu-rhcos-rhel8?tag=v3.12.8-1"
+    expected_cpe = ["cpe:/a:redhat:quay:3:*:el8:*"]
+    _check_node_names_at_depth(result[0], 1, expected_cpe)
 
 
-def test_build_root_tree_dependency():
-    base_purl = "pkg:pypi/chardet"
+def test_trees_with_cpes_dependency():
     with open("tests/testdata/chardet.json") as file:
         data = json.load(file)
-    result = _build_root_tree(base_purl, data)
-    _render_tree(result)
-    assert result.name == base_purl
+    result = _trees_with_cpes(data)
+    assert len(result) == 1
+    _render_tree(result[0])
     _check_node_names_at_depth(
-        result, 2, ["pkg:oci/quay-builder-qemu-rhcos-rhel8?tag=v3.12.8-1"]
+        result[0], 2, ["pkg:oci/quay-builder-qemu-rhcos-rhel8?tag=v3.12.8-1"]
     )
     expected_cpe = ["cpe:/a:redhat:quay:3:*:el8:*"]
-    _check_node_names_at_depth(result, 3, expected_cpe)
+    _check_node_names_at_depth(result[0], 3, expected_cpe)
 
 
-def test_build_root_tree_spdx_dependency():
-    base_purl = "pkg:nuget/NGX"
+def test_trees_with_cpes_spdx_dependency():
     with open("tests/testdata/NGX.json") as file:
         data = json.load(file)
-    result = _build_root_tree(base_purl, data)
-    _render_tree(result)
-    assert result.name == base_purl
+    result = _trees_with_cpes(data)
+    assert len(result) == 1
+    _render_tree(result[0])
     _check_node_names_at_depth(
-        result, 3, ["cpe:/a:redhat:enterprise_linux_ai:1.4:*:el9:*"]
+        result[0], 3, ["cpe:/a:redhat:enterprise_linux_ai:1.4:*:el9:*"]
     )
+
+
+def test_trees_with_cpes_multi_versions():
+    with open("tests/testdata/quay-builder-qemu-multi.json") as file:
+        data = json.load(file)
+    result = _trees_with_cpes(data)
+    assert len(result) == 2
+
+    print("first_result")
+    _render_tree(result[0])
+    assert result[0].name == "pkg:oci/quay-builder-qemu-rhcos-rhel8?tag=v3.14.0-4"
+    expected_cpes = ["cpe:/a:redhat:quay:3:*:el8:*"]
+    _check_node_names_at_depth(result[0], 1, expected_cpes)
+
+    print("second_result")
+    _render_tree(result[1])
+    assert result[1].name == "pkg:oci/quay-builder-qemu-rhcos-rhel8?tag=v3.12.8-1"
+    _check_node_names_at_depth(result[1], 1, expected_cpes)
 
 
 def _check_node_names_at_depth(result, depth, expected):
@@ -112,32 +124,61 @@ def _check_node_names_at_depth(result, depth, expected):
     assert sorted(expected) == sorted(node_names)
 
 
-def test_consolidate_duplicate_nodes():
-    # Create a sample tree
-    root = Node("root")
-    create_node(root, "A", ["A1", "A2"])
-    create_node(root, "B", ["B1", "B2"])
-    create_node(root, "A", ["A3"])
-
-    _render_tree(root)
-
-    # Call the function
-    consolidated_root = _consolidate_duplicate_nodes(root)
-
-    _render_tree(consolidated_root)
-
-    # Check if the root node remains the same
-    assert consolidated_root.name == "root"
-    # Check if the first node A is consolidated
-    assert len(consolidated_root.children) == 2
-    # Check if the children of node A are still present
-    consolidated_a_node = consolidated_root.children[0]
-    assert consolidated_a_node.children[0].name == "A1"
-    assert consolidated_a_node.children[1].name == "A2"
-    assert consolidated_a_node.children[2].name == "A3"
+def test_has_cpe_node_with_cpe_name():
+    root = Node("cpe:/a", children=[Node("cpe:/b"), Node("d")])
+    assert _has_cpe_node(root)
 
 
-def create_node(root, name, children):
+def test_has_cpe_node_without_cpe_name():
+    root = Node("d", children=[Node("cpe:/a"), Node("b")])
+    assert _has_cpe_node(root)
+
+
+def test_has_cpe_node_with_cpe_descendant():
+    root = Node("d", children=[Node("b"), Node("cpe:/a")])
+    assert _has_cpe_node(root)
+
+
+def test_has_cpe_node_with_multiple_cpe_descendants():
+    root = Node("d", children=[Node("b"), Node("cpe:/a"), Node("cpe:/b")])
+    assert _has_cpe_node(root)
+
+
+def test_has_cpe_node_with_no_descendants():
+    root = Node("d")
+    assert not _has_cpe_node(root)
+
+
+def test_has_cpe_node_with_empty_children():
+    root = Node("d")
+    assert not _has_cpe_node(root)
+
+
+# TODO make this pass as well
+# def test_remove_duplicate_parent_nodes():
+#     # Create a tree with duplicate parent nodes
+#     root = Node("root")
+#     child1 = Node("child1", parent=root)
+#     child2 = Node("child1", parent=child1)
+#     child3 = Node("child1", parent=child2)
+#     Node("grandchild1", parent=child3)
+#
+#     _render_tree(root)
+#
+#     # Call the function to remove duplicate parent nodes
+#     _remove_duplicate_parent_nodes(root)
+#
+#     print(f"tree after remving duplicate parent nodes:")
+#     _render_tree(root)
+#
+#     # Assert that the tree structure is as expected
+#     _check_node_names_at_depth(root, 1, ["child1"])
+#     _check_node_names_at_depth(root, 2, ["grandchild1"])
+
+
+def create_node(root, name, children) -> list[Node]:
     node_a = Node(name, parent=root)
+    new_nodes = []
     for child in children:
-        Node(child, parent=node_a)
+        new_nodes.append(Node(child, parent=node_a))
+    return new_nodes
