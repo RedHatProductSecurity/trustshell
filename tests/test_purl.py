f82d9fc465cb8cc1ed470734e8c7d557480b96f3
Jason Shepherd <jason@jasonshepherd.net>
2025-05-02 14:42:52 +1000

    add auth

--- tests/test_purl.py
+++ tests/test_purl.py
@@ -9,4 +9,4 @@ def test_package_versions(mock_lookup):
     expected_output = {"v3.12.8-1", "v3.12.8", "v3.12"}
     with open("tests/testdata/base_purl-quay-builder-qemu-rhcos-rhel-8.json") as file:
         mock_lookup.return_value = json.load(file)
-    assert _get_package_versions(base_purl) == expected_output
+    assert _get_package_versions(base_purl, {}) == expected_output
